d923b21303c5e254dfe4e391c6d591fca471cf8a
Kyna <khn710@gmail.com>
2015-06-10 14:25:50 -0700

    add create.rb file

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './TaskList'
+run TaskList
---
+++ db_create.rb
@@ -0,0 +1,6 @@
+require 'sqlite3'
+
+db = SQLite3::Database.new "testing.db"
+db execute "CREATE TABLE data (id INTEGER PRIMARY KEY, name TEXT NOT NULL, description TEXT, date TEXT);"
+
+db.close if db
