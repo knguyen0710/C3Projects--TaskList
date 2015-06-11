cc957976381d998a30a69696b573813f1508ad30
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-06-10 17:00:42 -0700

    Working on connecting instances to methods.

--- lib/task_record.rb
+++ lib/task_record.rb
@@ -3,8 +3,21 @@ require 'sqlite3'
 module TaskList
   class TaskRecord < Database
     def your_custom_query_here(*args)
+      # args = [{"name"=>"B", "description"=>"D", "date"=>"C"}]
+      # args[0] = {"name"=>"B", "description"=>"D", "date"=>"C"}
+      @name = args[0]["name"]
+      @description = args[1]["description"]
+      @date = args[2]["date"]
 
+
+
+      # santitize/validate your arguments
+
+      # prepare your statement
+
+      # call `query!` to interact with the database
+
+      # determine what should be returned
     end
-  
   end
 end
--- my-site.rb
+++ my-site.rb
@@ -1,10 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
+# require "./lib/task_record"
+# require "./lib/database"
 
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
+    erb :home
+  end
+
+  post "/" do
+    @database_object = TaskList::TaskRecord.new("taskList.db")
+
+    @database_object.your_custom_query_here(params)
+
+    # task_object = TaskList::TaskRecord.new("taskList.db")
+    # task_object.your_custom_query_here(ARGUMENTS)
     erb :home
   end
 
