7145ae08beffcbe1264f0bcbea9e68f379585560
Kyna <khn710@gmail.com>
2015-06-15 15:43:58 -0700

    added stylings

--- my-site.rb
+++ my-site.rb
@@ -6,6 +6,9 @@ require "./lib/task_record"
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
+  set :raise_errors, false
+  set :show_exceptions, false
+
   get "/" do
     data = TaskList::TaskRecord.new("taskList.db")
     @tasks = data.all_tasks
@@ -25,10 +28,18 @@ class MySite < Sinatra::Base
 
     @name = params[:name]
     @description = params[:description]
-    @date = params[:date]      
+    @date = params[:date]
 
     @database_object.your_custom_query_here(@name, @description, @date)
     redirect "/"
+  end
+
+  get "/error" do
+    erb :error
+  end
+
+  error ArgumentError do
+    "INVALID INPUT: " + env['sinatra.error'].message
   end
 
 end
